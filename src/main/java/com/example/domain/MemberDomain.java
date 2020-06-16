2453cb09fa91aaa40d37582a01eabba9a3281548
YutaSawagami <yuta.sawagami@rakus-partners.co.jp>
2020-06-16 14:54:13 +0900

    ex05のmemberDomainを作成

---
+++ src/main/java/com/example/domain/MemberDomain.java
@@ -0,0 +1,34 @@
+package com.example.domain;
+
+public class MemberDomain {
+
+	private int id;
+	private String name;
+	private int age;
+	private int depId;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getDepId() {
+		return depId;
+	}
+	public void setDepId(int depId) {
+		this.depId = depId;
+	}
+	
+}
